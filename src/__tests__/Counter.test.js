533b45ad2613b8300a16d254c2eeebe19b05adc2
Amol <amol.b@bahwancybertek.com>
2024-10-18 00:00:55 +0530

    name change

--- src/__tests__/Counter.test.js
+++ src/__tests__/Counter.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Counter from './../Counter';
+import Counter from '../Counter';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
 
